1b6be2831dad0a7be16528ccbf3d6c4058150dde
TraceRicholson <trace.richolson@spaceforce.mil>
2021-06-21 11:38:09 -0500

    fix nav bar for AWS

--- src/components/rover-photos/spirit.js
+++ src/components/rover-photos/spirit.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { Link, NavLink } from 'react-router-dom'
 import '../roverphotos.css'
-import '../../nav.style.css'
+// import '../../nav.style.css'
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
